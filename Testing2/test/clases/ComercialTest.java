27be58e6a6125f7b54f0d19f43e4f60abbec6495
Babsrm <babsruizm@gmail.com>
2023-02-14 14:05:52 +0100

    ultima actualizacion añadida

--- Testing2/test/clases/ComercialTest.java
+++ Testing2/test/clases/ComercialTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ComercialTest {
+public class ComercialTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
